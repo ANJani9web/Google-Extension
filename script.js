cde819bec58ad15dbe1134f6a7925473b6760516
Anjani <anjanison9@gmail.com>
2023-06-27 12:55:17 +0530

    Air Pollution Info done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Retrieve weather data from OpenWeatherMap API
 
-//let city = prompt("What is your city name");
+let city = "Varanasi";
 
 
 function insertAfter(newElement, referenceElement) {
@@ -21,7 +21,7 @@ document.querySelector('.form_1').addEventListener('submit', function(event) {
 
   // Get the values entered by the user
   var name = document.getElementById('name').value;
-  var city = document.getElementById('city').value;
+  city = document.getElementById('city').value;
 
   // Display a personalized welcome message
   //var welcomeMessage = "Welcome, " + name + "! You are from " + city + ".";
@@ -99,7 +99,7 @@ fetch("https://type.fit/api/quotes")
     insertAfter(weather_div,temp);
     insertAfter(air_div,weather_div);
 
-    getWeatherData(25,86,weather_div)
+    getWeatherData(weather_div)
      //getPollutionData(25,86,weather_div)
     // insertAfter(weather_btn,air_div);
     // insertAfter(air_btn,weather_btn);
@@ -108,9 +108,9 @@ fetch("https://type.fit/api/quotes")
     // var weather_btn = document.getElementById('weather_btn_id');
     // weather_btn.onclick=getWeatherData(25,86,weather_div);
     //weather_btn.onclick=getWeatherData(25,86,weather_div);
-
+    console.log(quote_m)
     temp.innerHTML = `<p style='color: red; font-size: 18px; padding-top:10px;'>A Quote for You</p>
-     " ${quote_m.text} " <br><br>   -By ${quote_m.author}`;
+     " ${quote_m.text} " <br><br>   - By ${quote_m.author}`;
     getPollutionData(25,86,air_div)
     // var weather_btn = document.getElementById('weather_btn_id');
     // weather_btn.onclick=getWeatherData(25,86,weather_div);
@@ -181,28 +181,101 @@ button.onclick = handleClick;
 
 
 
-function getWeatherData(latitude, longitude,divElement) {
+function getWeatherData(divElement) {
   const apiKey = "08acd3959fc64db2afa65937232406";
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=08acd3959fc64db2afa65937232406&q=Varanasi&days=4&aqi=yes&alerts=yes`;
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=08acd3959fc64db2afa65937232406&q=${city}&days=4&aqi=yes&alerts=yes`;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
       // Extract weather information from the API response
-      const temperature = data.current.temp_c;
+      
+      // location
+      const loaction_name = data.location.name;
+      const location_country = data.location.country;
+      const location_lat = data.location.lat;
+      const location_lon = data.location.lon;
+      
+      // Astro info
+      const sunrise = data.forecast.forecastday[0].astro.sunrise;
+      const sunset = data.forecast.forecastday[0].astro.sunset;
+      const moonrise = data.forecast.forecastday[0].astro.moonrise;
+      const moonset = data.forecast.forecastday[0].astro.moonset;
+      const moon_phase = data.forecast.forecastday[0].astro.moon_phase;
+      const moon_illumination = data.forecast.forecastday[0].astro.moon_illumination;
+
+      // Temperature info
+      const current_temp = data.current.temp_c;
+      const max_temp = data.forecast.forecastday[0].day.maxtemp_c;
+      const min_temp = data.forecast.forecastday[0].day.mintemp_c;
+      const avg_temp = data.forecast.forecastday[0].day.avgtemp_c;
+      const feels_like = data.current.feelslike_c;
+      
+      // Wind info
+      const wind_speed = data.current.wind_kph;
+      const wind_degree = data.current.wind_degree;
+      const wind_dir = data.current.wind_dir;
+      const max_wind_speed = data.forecast.forecastday[0].day.maxwind_kph;
+      
+      // Rain info 
       const humidity = data.current.humidity;
-      const windSpeed = data.current.wind_kph;
-      
-      
+      const cloud = data.current.cloud;
+      const chance_of_rain = data.forecast.forecastday[0].day.daily_chance_of_rain;
+      const chance_of_snow = data.forecast.forecastday[0].day.daily_chance_of_snow;
+      const total_precipitation = data.forecast.forecastday[0].day.totalprecip_mm;
+      const condition = data.forecast.forecastday[0].day.condition.text;
+
+
       // Update the weather-info element in popup.html with the weather data
       divElement.innerHTML = `
         <div class="weather_report">Weather Report</div>
-        Temperature: ${temperature} &deg;C 
-        <br>
-        Humidity: ${humidity} %
-       <br>
-        Wind Speed: ${windSpeed} km/hr
-        <br>
+           
+           <p class="header">Location Info</p>
+           <div class="location">
+           Location : ${loaction_name}<br>
+           Country : ${location_country}<br>
+           Latitude : ${location_lat} <br>
+           Longitude : ${location_lon} 
+           </div>
+
+           <p class="header">Astro Info</p>
+           <div class="astro">
+            Sunrise : ${sunrise} <br>
+            Sunset : ${sunset} <br>
+            Moonrise : ${moonrise} <br>
+            Moonset : ${moonset} <br>
+            Moon Phase : ${moon_phase} <br>
+            Moon Illumination : ${moon_illumination} <br><br>
+           </div>
+
+           <p class="header">Temperature Info</p> 
+           <div class="temp">
+            Current Temperature : ${current_temp} &deg;C<br>
+            Max Temperature : ${max_temp} &deg;C<br>
+            Min Temperature : ${min_temp} &deg;C<br>
+            Average Temperature : ${avg_temp} &deg;C<br>
+            Feels Like : ${feels_like} &deg;C<br><br>
+           </div>
+           
+           <p class="header">Wind Info</p>
+           <div class="wind">
+            Wind Speed : ${wind_speed} km/hr<br>
+            Wind Degree : ${wind_degree}<br>
+            Wind Direction : ${wind_dir}<br>
+            Max Wind Speed : ${max_wind_speed} km/hr<br><br>
+           </div>
+
+           <p class="header">Rain Info</p>
+           <div class="rain">
+            Humidity : ${humidity}<br>
+            Cloud : ${cloud}<br>
+            Chance of Rain : ${chance_of_rain} %<br>
+            Chance of Snow : ${chance_of_snow} %<br>
+            Total Precipitation : ${total_precipitation} mm<br>
+            Condition : ${condition}<br><br>
+           </div>
+           
+  
         
       `;
     })
@@ -218,47 +291,42 @@ function getWeatherData(latitude, longitude,divElement) {
 function getPollutionData(latitude, longitude,divElement) {
   const apiKey = "ae01209a36db2619525812602750871b";
   // alert(city);
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=08acd3959fc64db2afa65937232406&q=Varanasi&days=4&aqi=yes&alerts=yes`;
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=08acd3959fc64db2afa65937232406&q=${city}&days=4&aqi=yes&alerts=yes`;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
       // Extract pollution information from the API response
-      console.log(data)
-      const components = data.current.air_quality.co;
-      let pollutionLevel = data.current.air_quality.no2;
-      let mainPollutant = data.current.air_quality.o3;
-      let solutions = data.current.air_quality.so2;
-      
-      const pollution_l = parseFloat(pollutionLevel);
-      const main_p = parseFloat(mainPollutant);
-      const solutions_l = parseFloat(solutions);
-
-      if(pollution_l > 0 && pollution_l < 50){
-        pollutionLevel = "Good";
-      }
-      else if(pollution_l > 51 && pollution_l < 100){
-        pollutionLevel = "Moderate";
-      }
-      else if(pollution_l > 101 && pollution_l < 150){
-        pollutionLevel = "Unhealthy for Sensitive Groups";
-      }
-      else if(pollution_l > 151 && pollution_l < 200){
-        pollutionLevel = "Unhealthy";
-
-      } 
-      else if(pollution_l > 201 && pollution_l < 300){
-        pollutionLevel = "Very Unhealthy";
-      }
-
-
+      //console.log(data)
+      const uv_concentration = data.current.uv;
+      const co_concentration = data.current.air_quality.co;
+      const no2_concentration = data.current.air_quality.no2;
+      const o3_concentration = data.current.air_quality.o3;
+      const so2_concentration = data.current.air_quality.so2;
+      const pm2_5_concentration = data.current.air_quality.pm2_5;
+      const pm10_concentration = data.current.air_quality.pm10;
+     
       // Update the pollution-info and solutions elements in popup.html with the pollution data
       divElement.innerHTML = `
       <div class="pollution_level">Pollution Level</div>
-       
-        Pollution Level: ${pollutionLevel}<br>
-        Main Pollutant: ${mainPollutant}
-      `;
+       &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+        Pollutant &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  Concentration &nbsp&nbsp&nbsp&nbsp   <br>
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+        <span class="pollutant">UV</span>   <span class="concentration uv">${uv_concentration.toFixed(2)}</span>   <br>
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+        <span class="pollutant">CO</span> <span class="concentration co">${co_concentration.toFixed(2)}</span>    <br>
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+        <span class="pollutant">NO2</span> <span class="concentration no2">${no2_concentration.toFixed(2)}</span> <br>
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+        <span class="pollutant">O3</span> <span class="concentration o3">${o3_concentration.toFixed(2)}</span>    <br>
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+        <span class="pollutant">SO2</span> <span class="concentration so2">${so2_concentration.toFixed(2)}</span>  <br>
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+        <span class="pollutant">PM 2.5</span> <span class="concentration pm25">${pm2_5_concentration.toFixed(2)}</span> <br>
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+        <span class="pollutant">PM 10</span> <span class="concentration pm10">${pm10_concentration.toFixed(2)}</span> <br>
+        <br>
+      `; 
       //document.getElementById("solutions_info").innerHTML = `Solutions: ${solutions}`;
     })
     .catch(error => {
